96ab2cc07bbc96772b1f5a5329cef70a00420fa8
Igor Lebovic <igor.lebovic@gmail.com>
2012-04-28 19:35:18 -0400

    added outta and into into member model and show views

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,14 +2,21 @@ class Member
 
 # require "./app/models/member.rb"
 
-  attr_reader :first_name, :last_name, :username, :password
+  attr_reader :first_name, :last_name, :username, :password, :outta, :into
   
   def self.all
-    @members ||= {:user1 => ["Igor", "Lebovic", "aerosmile", "testing"],
-                  :user2 => ["John", "Miller", "mr_awesome", "testing"]
-                  }.map { |k, v| new(first_name: v[0], last_name: v[1], username: v[2], password: v[3]) }
+    @members ||= {:user1 => ["Igor", "Lebovic", "igor", "testing", ["john", "jason"], ["jason", "jeff"]],
+                  :user2 => ["John", "Miller", "john", "testing", ["igor", "jason"], ["igor", "jeff"]],
+                  :user3 => ["Jason", "Miller", "jason", "testing", ["john", "jeff"], ["igor", "jeff"]],
+                  :user4 => ["Jeff", "Miller", "jeff", "testing", ["john", "jason"], ["igor", "jason"]]
+                  }.map { |k, v| new(first_name: v[0],
+                                     last_name: v[1],
+                                     username: v[2],
+                                     password: v[3],
+                                     outta: v[4],
+                                     into: v[5]) }
   end
-  
+
   def self.find(param)
     all.detect { |l| l.username.to_param == param } || raise(ActiveRecord::RecordNotFound)
   end
@@ -19,6 +26,8 @@ class Member
     @last_name = options[:last_name]
     @username = options[:username]
     @password = options[:password]
+    @outta = options[:outta]
+    @into = options[:into]
   end
   
 end
