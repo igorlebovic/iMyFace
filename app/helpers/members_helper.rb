0b74fe283d5aefcc30f7dd56003f9afa3d5945cb
Igor Lebovic <igor.lebovic@gmail.com>
2012-04-29 16:00:48 -0400

    should be working but displays blank

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -3,8 +3,9 @@ module MembersHelper
     if member.outta.any?
       member.outta.each do |username|
         user = Member.find(username)
-        render(:partial => "members/user", :locals => {:user => user}) + nested_outta(user)
-      end.join.html_safe
+        render(:partial => "members/user", :locals => {:user => user})
+         # + nested_outta(user)
+      end
     end
   end
 end
