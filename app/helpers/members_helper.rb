0353a3032bd4ea69ad6dde3c62cf4cf4d9d85b87
Igor Lebovic <igor.lebovic@gmail.com>
2012-04-29 20:47:13 -0400

    correctly listing outta's of first item in each loop, as many layers deep as necessary

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,10 +1,18 @@
 module MembersHelper
   def nested_outta(member)
     if member.outta.any?
-      member.outta.each do |username|
-        user = Member.find(username)
-        render(:partial => "members/user", :locals => {:user => user}) + nested_outta(user)
-      end.join.html_safe
+      user = Member.find(member.outta.first)
+      render(:partial => "user", :locals => {:user => user}) + nested_outta(user)
     end
   end
 end
+
+
+# member.outta.each do |username|
+#         user = Member.find(username)
+#         # redirect_to root_path
+#         # render :text => "OK"
+#         # render "user"
+#         # nested_outta(user)
+#         render :partial => "user", :locals => {:user => user}
+#       end.join.html_safe
