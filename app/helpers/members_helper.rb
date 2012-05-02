3e03cfd55f2fb0ae5713f0da845702f9af66329b
Igor Lebovic <igor.lebovic@gmail.com>
2012-05-02 17:54:00 -0400

    ugly code with vertical hash capability

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,5 +1,6 @@
 module MembersHelper
   def nested(members)
+    # raise members.inspect
     members.map do |key, value|
       render(:partial => "user", :locals => {:user => key}) + content_tag(:div, nested(value), :class => "nested")
     end.join.html_safe
--- app/models/member.rb
+++ app/models/member.rb
@@ -29,13 +29,74 @@ class Member
     @into = options[:into]
   end
   
-  def hash_outta(i=0)
-    outters = Hash.new
-    if self.outta.any?
-      self.outta.each do |username|
-        user = Member.find(username)
-        outters[user] = user.hash_outta(i)
+  def hash_outta
+    member = self
+    # does the member have any followers?
+    if member.outta.any?
+      # create a hash with member's first-order followers
+      hash = followers(member)
+      # iterate over each first-order follower
+      hash.each_key do |member|
+        # does the first-order follower have any followers?
+        if member.outta.any?
+          # create a hash with follower's second-order followers
+          sub_hash = followers(member)
+          # insert a hash of second-order followers as a value of the first-order follower's key
+          hash[member].replace(sub_hash).each_key do |member2|
+            if member2.outta.any?
+              sub_hash = followers(member2)
+              hash[member][member2].replace(sub_hash).each_key do |member3|
+                if member3.outta.any?
+                  sub_hash = followers(member3)
+                  hash[member][member2][member3].replace(sub_hash).each_key do |member4|
+                    if member4.outta.any?
+                      sub_hash = followers(member4)
+                      hash[member][member2][member3][member4].replace(sub_hash).each_key do |member5|
+                        if member5.outta.any?
+                          sub_hash = followers(member5)
+                          hash[member][member2][member3][member4][member5].replace(sub_hash).each_key do |member6|
+                            if member6.outta.any?
+                              sub_hash = followers(member6)
+                              hash[member][member2][member3][member4][member5][member6].replace(sub_hash).each_key do |member7|
+                                if member7.outta.any?
+                                  sub_hash = followers(member7)
+                                  hash[member][member2][member3][member4][member5][member6][member7].replace(sub_hash).each_key do |member8|
+                                    if member8.outta.any?
+                                      sub_hash = followers(member8)
+                                      hash[member][member2][member3][member4][member5][member6][member7][member8]
+                                      # base = { user = { user = {}}}
+                                      # cur = base
+                                      # cur = cur[user]
+                                    end
+                                  end
+                                end
+                              end
+                            end
+                          end
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
       end
+    end
+    return hash
+  end
+
+
+  def followers(member)
+    # initialize a hash of first-order followers
+    outters = Hash.new
+    # iterate over members' followers
+    member.outta.each do |username|
+      # associate the follower's username with the object
+      user = Member.find(username)
+      # in the hash of first-order followers, object is the key and an empty hash is the value
+      outters[user] = Hash.new
     end
     return outters
   end
