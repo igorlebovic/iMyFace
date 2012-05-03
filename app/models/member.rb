1ff0eb2e7c9c2e4207c0e9990584c391c70ab133
Igor Lebovic <igor.lebovic@gmail.com>
2012-05-03 01:00:32 -0400

    yay

--- app/models/member.rb
+++ app/models/member.rb
@@ -28,72 +28,24 @@ class Member
     @excluded_outta = Set.new
     if member.outta.any?
       hash = followers(member)
-      hash.each_key do |member|
-        if member.outta.any?
-          sub_hash = followers(member)
-          hash[member].replace(sub_hash).each_key do |member2|
-            if member2.outta.any?
-              sub_hash = followers(member2)
-              hash[member][member2].replace(sub_hash).each_key do |member3|
-                if member3.outta.any?
-                  sub_hash = followers(member3)
-                  hash[member][member2][member3].replace(sub_hash).each_key do |member4|
-                    if member4.outta.any?
-                      sub_hash = followers(member4)
-                      hash[member][member2][member3][member4].replace(sub_hash).each_key do |member5|
-                        if member5.outta.any?
-                          sub_hash = followers(member5)
-                          hash[member][member2][member3][member4][member5].replace(sub_hash).each_key do |member6|
-                            if member6.outta.any?
-                              sub_hash = followers(member6)
-                              hash[member][member2][member3][member4][member5][member6].replace(sub_hash).each_key do |member7|
-                                if member7.outta.any?
-                                  sub_hash = followers(member7)
-                                  hash[member][member2][member3][member4][member5][member6][member7].replace(sub_hash).each_key do |member8|
-                                    if member8.outta.any?
-                                      sub_hash = followers(member8)
-                                      hash[member][member2][member3][member4][member5][member6][member7][member8].replace(sub_hash).each_key do |member9|
-                                        if member9.outta.any?
-                                          sub_hash = followers(member9)
-                                          hash[member][member2][member3][member4][member5][member6][member7][member8][member9].replace(sub_hash).each_key do |member10|
-                                            if member10.outta.any?
-                                              sub_hash = followers(member10)
-                                              hash[member][member2][member3][member4][member5][member6][member7][member8][member9][member10].replace(sub_hash).each_key do |member11|
-                                                if member11.outta.any?
-                                                  sub_hash = followers(member11)
-                                                  hash[member][member2][member3][member4][member5][member6][member7][member8][member9][member10][member11].replace(sub_hash).each_key do |member12|
-                                                    if member12.outta.any?
-                                                      sub_hash = followers(member12)
-                                                      hash[member][member2][member3][member4][member5][member6][member7][member8][member9][member10][member11][member12]
-                                                    end
-                                                  end
-                                                end
-                                              end
-                                            end
-                                          end
-                                        end
-                                      end
-                                    end
-                                  end
-                                end
-                              end
-                            end
-                          end
-                        end
-                      end
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
+      recursive(hash)
     end
     return hash
   end
-
-  # raise @excluded.inspect
+  
+  def recursive(hash)
+    hash.each_key do |member|
+      if member.outta.any?
+        sub_hash = followers(member)
+        # raise hash.inspect
+        # raise member.inspect
+        # raise hash[member].inspect
+        # hash = hash[member]
+        recursive(hash[member].replace(sub_hash))
+      end
+    end
+  end
+  
   
   # base = { user = { user = {}}}
   # cur = base
