7e5c2bf459786684605a9e43ade2db5c3b955cbc
Igor Lebovic <igor.lebovic@gmail.com>
2012-05-05 00:57:20 -0400

    ugly but works perfectly

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,22 +1,41 @@
 module MembersHelper
-  def nested(members)
+  def nested(members, i=0)
+    i = i + 1
     members.map do |key, value|
-      render(:partial => "user", :locals => {:user => key}) + content_tag(:div, nested(value), :class => "nested")
+      render(:partial => "user", :locals => {:user => key, :i => i}) + content_tag(:div, nested(value, i), :class => "nested")
     end.join.html_safe
   end
 end
 
 def hash_outta(member)
   @excluded_outta = Set.new
-    hash = member_followers(member)
-    sub_followers(hash)
+  hash = member_followers(member)
+  sub_followers(hash)
+end
+
+def hash_into(member)
+  @excluded_into = Set.new
+  hash = member_following(member)
+  sub_following(hash)
 end
 
 def member_followers(member)
   hash = Hash.new
   member.outta.each do |username|
     unless @excluded_outta.include?(username)
-      @excluded_outta.add(username)      
+      @excluded_outta.add(username)
+      user = Member.find(username)
+      hash[user] = Hash.new
+    end
+  end
+  return hash
+end
+
+def member_following(member)
+  hash = Hash.new
+  member.into.each do |username|
+    unless @excluded_into.include?(username)
+      @excluded_into.add(username)      
       user = Member.find(username)
       hash[user] = Hash.new
     end
@@ -29,27 +48,114 @@ def sub_followers(hash)
     sub_hash = member_followers(member)
     hash[member].replace(sub_hash)
   end
-  hash.each_pair do |key, follower_hash|
+  hash.each do |key, follower_hash|
     follower_hash.each_key do |follower_member|
       sub_hash = member_followers(follower_member)
-      sub_followers(hash[key][follower_member].replace(sub_hash))
+      hash[key][follower_member].replace(sub_hash)
     end
-    # place rec function here
   end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each_key do |follower_member|
+        sub_hash = member_followers(follower_member)
+        hash[key][key2][follower_member].replace(sub_hash)
+      end
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each do |key3, follower_hash3|
+        follower_hash3.each_key do |follower_member|
+          sub_hash = member_followers(follower_member)
+          hash[key][key2][key3][follower_member].replace(sub_hash)
+        end
+      end
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each do |key3, follower_hash3|
+        follower_hash3.each do |key4, follower_hash4|
+          follower_hash4.each_key do |follower_member|
+            sub_hash = member_followers(follower_member)
+            hash[key][key2][key3][key4][follower_member].replace(sub_hash)
+          end
+        end
+      end
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each do |key3, follower_hash3|
+        follower_hash3.each do |key4, follower_hash4|
+          follower_hash4.each do |key5, follower_hash5|
+            follower_hash5.each_key do |follower_member|
+              sub_hash = member_followers(follower_member)
+              hash[key][key2][key3][key4][key5][follower_member].replace(sub_hash)
+              raise hash.inspect
+            end
+          end
+        end
+      end
+    end
+  end
+  
 end
 
-# member_followers
-# 1=>{}
-# 2=>{}
-# 3=>{}
-# 
-# sub_followers
-# 1=>{1=>{},
-#     2=>{},
-#     3=>{}}
-# 2=>{1=>{},
-#     2=>{},
-#     3=>{}}
-# 3=>{1=>{},
-#     2=>{},
-#     3=>{}}
+def sub_following(hash)
+  hash.each_key do |member|
+    sub_hash = member_following(member)
+    hash[member].replace(sub_hash)
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each_key do |follower_member|
+      sub_hash = member_following(follower_member)
+      hash[key][follower_member].replace(sub_hash)
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each_key do |follower_member|
+        sub_hash = member_following(follower_member)
+        hash[key][key2][follower_member].replace(sub_hash)
+      end
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each do |key3, follower_hash3|
+        follower_hash3.each_key do |follower_member|
+          sub_hash = member_following(follower_member)
+          hash[key][key2][key3][follower_member].replace(sub_hash)
+        end
+      end
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each do |key3, follower_hash3|
+        follower_hash3.each do |key4, follower_hash4|
+          follower_hash4.each_key do |follower_member|
+            sub_hash = member_following(follower_member)
+            hash[key][key2][key3][key4][follower_member].replace(sub_hash)
+          end
+        end
+      end
+    end
+  end
+  hash.each do |key, follower_hash|
+    follower_hash.each do |key2, follower_hash2|
+      follower_hash2.each do |key3, follower_hash3|
+        follower_hash3.each do |key4, follower_hash4|
+          follower_hash4.each do |key5, follower_hash5|
+            follower_hash5.each_key do |follower_member|
+              sub_hash = member_following(follower_member)
+              hash[key][key2][key3][key4][key5][follower_member].replace(sub_hash)
+              raise hash.inspect
+            end
+          end
+        end
+      end
+    end
+  end
+end
